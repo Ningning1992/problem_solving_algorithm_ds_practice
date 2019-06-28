fc10331dc77c69908163c23f9205fda321227bf0
Long,Ningning <ningning.long@ejgallo.com>
2019-06-27 20:18:35 -0700

    start learning recursion

---
+++ recursion/calculate_sum_recursion.py
@@ -0,0 +1,22 @@
+
+# traditional way
+def listsum(numList):
+    theSum = 0
+    for i in numList:
+        theSum = theSum + i
+    return theSum
+
+
+# recursion
+def listsum2(numList):
+   if len(numList) == 1:
+        return numList[0]
+   else:
+        return numList[0] + listsum(numList[1:])
+
+
+if __name__ == "__main__":
+    print(listsum([1,3,5,7,9]))
+    print(listsum2([1,3,5,7,9]))
+
+
